e4dee836b2f7f1b8cca8ee14b2a11d28f20cca06
dkool <dennis.kool@cloudnation.nl>
2022-12-02 15:54:37 +0100

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,6 @@ output "vaults" {
   value = azurerm_key_vault.keyvault
 }
 
-output "kv_keys" {
-  value = azurerm_key_vault_key.kv_keys
+output "keys" {
+  value = values(azurerm_key_vault_key.kv_keys)[*].id
 }
-
-# output "kv_keys" {
-#   value = flatten([for m in azurerm_key_vault_key.kv_keys :
-#     [for k, v in m : v]
-#   ])
-# }
