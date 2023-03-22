abcc66256cc181348a9f53a4aa9b152db6083f39
dkool <dennis.kool@cloudnation.nl>
2023-03-22 18:50:32 +0100

    feat: add tls private key support

--- outputs.tf
+++ outputs.tf
@@ -6,12 +6,14 @@ output "kv_keys" {
   value = azurerm_key_vault_key.kv_keys
 }
 
+output "random_string" {
+  value = azurerm_key_vault_secret.secret
+}
+
+output "tls_public_key" {
+  value = azurerm_key_vault_secret.tls_secret
+}
+
 # output "merged_ids" {
 #   value = values(azurerm_key_vault.keyvault)[*].id
-# }
-
-# output "kv_keys" {
-#   value = flatten([for m in var.module_ids :
-#     [for k, v in m : v]
-#   ])
 # }
